0ee0cd632fa3e02edb542a895812b46d758a8275
AndrewEnot <103563559+AndrewEnot@users.noreply.github.com>
2022-09-28 17:12:53 +0300

    Last commit

---
+++ javapro/src/homework10/Main.java
@@ -0,0 +1,10 @@
+package homework10;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    ValueCalculator calculator = new ValueCalculator(50000);
+    calculator.doCalc(1);
+  }
+}
--- javapro/src/homework10/ValueCalculator.java
+++ javapro/src/homework10/ValueCalculator.java
@@ -1,5 +1,45 @@
 package homework10;
 
-public class ValueCalculator {
+import java.util.Arrays;
 
+public class ValueCalculator extends Thread {
+
+  float[] array;
+  int halfarrayLength;
+
+  public ValueCalculator(int halfarrayLength) {
+    this.halfarrayLength = halfarrayLength;
+    this.array = new float[halfarrayLength * 2];
+  }
+
+  public void doCalc(float j) {
+    float[] arrayOne = new float[halfarrayLength];
+    float[] arrayTwo = new float[halfarrayLength];
+    long start = System.currentTimeMillis();
+    System.out.println("Calculation starts at: " + start + " .......");
+
+    Arrays.fill(array, j);
+    System.arraycopy(array, 0, arrayOne, 0, halfarrayLength);
+    System.arraycopy(array, halfarrayLength - 1, arrayTwo, 0, halfarrayLength);
+
+    MyThread thread1 = new MyThread(arrayOne);
+    MyThread thread2 = new MyThread(arrayTwo);
+
+    try {
+      thread1.start();
+      thread2.start();
+
+      thread1.join();
+      thread2.join();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+    System.arraycopy(arrayOne, 0, array, 0, halfarrayLength);
+    System.arraycopy(arrayTwo, 0, array, halfarrayLength - 1, halfarrayLength);
+
+    long finish = System.currentTimeMillis();
+    System.out.println("Calculating finished time: " + finish);
+    long usedtime = finish - start;
+    System.out.println("Used time: " + usedtime + " ms.");
+  }
 }
